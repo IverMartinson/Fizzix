c68968f639fc104c2e28085dc5a8de52bc06cb27
Iver <iver.m.martinson@gmail.com>
2026-01-25 16:06:33 -0500

    updated example scene

--- src/launch program/main.c
+++ src/launch program/main.c
@@ -11,30 +11,66 @@ int main(){
     FZ_shape* shape_1 = FZ_new_shape();
     FZ_shape* shape_2 = FZ_new_shape();
     FZ_shape* shape_3 = FZ_new_shape();
+    FZ_shape* shape_4 = FZ_new_shape();
+    FZ_shape* shape_5 = FZ_new_shape();
+    FZ_shape* shape_6 = FZ_new_shape();
+    FZ_shape* shape_7 = FZ_new_shape();
+    FZ_shape* shape_8 = FZ_new_shape();
+    FZ_shape* shape_9 = FZ_new_shape();
+    FZ_shape* shape_10 = FZ_new_shape();
+    FZ_shape* shape_11 = FZ_new_shape();
 
     scene->shapes = malloc(sizeof(FZ_shape*) * 3);
 
     scene->shapes[0] = shape_1;
     scene->shapes[1] = shape_2;
     scene->shapes[2] = shape_3;
+    scene->shapes[3] = shape_4;
+    scene->shapes[4] = shape_5;
+    scene->shapes[5] = shape_6;
+    scene->shapes[6] = shape_7;
+    scene->shapes[7] = shape_8;
+    scene->shapes[8] = shape_9;
+    scene->shapes[9] = shape_10;
+    scene->shapes[10] = shape_11;
 
     shape_2->flags = FZ_SHAPE_IS_STATIC;
 
     shape_1->scale = (FZ_vector_2){100, 20};
     shape_3->scale = (FZ_vector_2){100, 20};
-    shape_2->scale = (FZ_vector_2){2000, 100};
+    shape_4->scale = (FZ_vector_2){100, 20};
+    shape_5->scale = (FZ_vector_2){100, 20};
+    shape_2->scale = (FZ_vector_2){700, 100};
+    shape_6->scale = (FZ_vector_2){50, 50};
+    shape_7->scale = (FZ_vector_2){50, 50};
+    shape_8->scale = (FZ_vector_2){50, 50};
+    shape_9->scale = (FZ_vector_2){50, 50};
+    shape_10->scale = (FZ_vector_2){50, 50};
+    shape_11->scale = (FZ_vector_2){50, 50};
 
     shape_1->position = (FZ_vector_2){-150, 50};
     shape_3->position = (FZ_vector_2){150, 50};
+    shape_4->position = (FZ_vector_2){150, 300};
+    shape_5->position = (FZ_vector_2){150, 200};
+    shape_6->position = (FZ_vector_2){500, 100};
+    shape_7->position = (FZ_vector_2){500, 150};
+    shape_8->position = (FZ_vector_2){500, 200};
+    shape_9->position = (FZ_vector_2){500, 250};
+    shape_10->position = (FZ_vector_2){500, 300};
+    shape_11->position = (FZ_vector_2){500, 350};
     shape_2->position = (FZ_vector_2){0, -150};
 
-    scene->shape_count = 3;
+    scene->shape_count = 11;
 
     shape_1->velocity = (FZ_vector_2){60, 300};
     shape_3->velocity = (FZ_vector_2){-60, 300};
+    shape_4->velocity = (FZ_vector_2){-60, -300};
+    shape_5->velocity = (FZ_vector_2){60, 300};
     
     shape_1->angular_velocity = 4;
     shape_3->angular_velocity = -5;
+    shape_4->angular_velocity = -1;
+    shape_5->angular_velocity = 3;
     // shape_2->angular_velocity = 2;
 
     shape_3->mass = 1;
